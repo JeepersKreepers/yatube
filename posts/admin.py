14c67d18b800b51a76bc12533a9a4dca1e1e6a44
vivashov <v.ivashov154@gmail.com>
2023-10-20 22:11:16 +0300

    project_1 end

--- posts/admin.py
+++ posts/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 # из файла models импортируем модель Post
-from .models import Post
+from .models import Group, Post
 
 
 class PostAdmin(admin.ModelAdmin):
@@ -13,6 +13,14 @@ class PostAdmin(admin.ModelAdmin):
     # это свойство сработает для всех колонок: где пусто - там будет эта строка
     empty_value_display = "-пусто-"
 
+class GroupAdmin(admin.ModelAdmin):
+    # перечисляем поля, которые должны отображаться в админке
+    list_display = ("pk", "title", "slug", "description",)
+    # добавляем интерфейс для поиска по тексту постов
+    search_fields = ("title",)
+    # это свойство сработает для всех колонок: где пусто - там будет эта строка
+    empty_value_display = "-пусто-"
 
 # при регистрации модели Post источником конфигурации для неё назначаем класс PostAdmin
 admin.site.register(Post, PostAdmin)
+admin.site.register(Group, GroupAdmin)
--- posts/models.py
+++ posts/models.py
@@ -3,6 +3,14 @@ from django.contrib.auth import get_user_model
 
 User = get_user_model()
 
+class Group(models.Model):
+    title = models.CharField(max_length=200)
+    slug =models.SlugField(unique=True)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
 class Post(models.Model):
     text = models.TextField()
     pub_date = models.DateTimeField("date published", auto_now_add=True)
@@ -12,3 +20,4 @@ class Post(models.Model):
 
     def __str__(self):
         return self.text
+
